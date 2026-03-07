f809ee5e6d5b5a6abc5e5d268a2de7478120554c
Faruuuqqq <faruqmahdison@gmail.com>
2026-03-07 23:48:33 +0700

    feat(frontend): create project structure (pages, components, lib, types)

---
+++ apps/frontend/src/pages/Index.tsx
@@ -0,0 +1,9 @@
+function Index() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">NutriGuard</h1>
+    </div>
+  )
+}
+
+export default Index
